8263b9f3c40a1aabd6be52369f66dbc94275e2df
Saswat <saswatm31@gmail.com>
2023-12-18 19:22:56 +0530

    final commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask,render_template,request,url_for,redirect
+app = Flask(__name__)
+
+@app.route('/',methods=["GET"])
+def welcome():
+    return '<h1>Welcome To My Page!<h1>'
+
+@app.route("/index",methods=["GET"])
+def index():
+    return "<h2>Welcome to the Index Page<h2>"
+
+#Variable rule
+@app.route('/success/<int:score>')
+def success(score):
+    return "The Person has passed and the score is: "+str(score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "The Person has failed and the score is: "+str(score)
+
+@app.route('/form',methods=["GET","POST"])
+def form():
+    if request.method=="GET":
+        return render_template('form.html')
+    else:
+        maths=float(request.form['maths'])
+        science=float(request.form['science'])
+        history=float(request.form['history'])
+
+        average_marks=(maths+science+history)/3
+        result=""
+        if average_marks>=50:
+            res="success"
+        else:
+            res="fail"
+        
+        return redirect(url_for(res,score=average_marks))
+        #return render_template('form.html',score=average_marks)
+
+
+if __name__=="__main__":
+    app.run(debug=True,port=8000)
